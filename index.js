e0a0e32993cf8323bc9d7f491c616f0d53b2b616
Benjamin Goza <bengoza@gmail.com>
2019-07-17 19:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ theBeatlesPlay();
 
 const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
-var newFacts=[];
+//var newFacts=[];
 
 function johnLennonFacts(facts) {
   var i = 0;
